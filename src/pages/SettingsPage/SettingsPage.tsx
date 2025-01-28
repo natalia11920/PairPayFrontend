526f9ed039f22fdb1f49c56f46d87fb6d1b4973e
ciasteczka01 <natalia.str19@onet.pl>
2025-01-28 10:02:54 +0100

    .

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -21,7 +21,7 @@ import {
 import { SearchIcon } from "../../components/SearchIcon/SearchIcon";
 import * as Yup from "yup";
 
-type Props = {}; // kom probny
+type Props = {}; 
 
 const UserPage = (props: Props) => {
   const [loggedUser, setLoggedUser] = useState<User | null>(null);
